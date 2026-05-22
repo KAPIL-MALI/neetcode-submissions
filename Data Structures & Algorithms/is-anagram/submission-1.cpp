497ce64691fd283556db5e30efa43ddfbeeb687d
KAPIL <kapilmali7789@gmail.com>
2026-05-22 18:27:16 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        int n1 = s.size(),n2 = t.size();
+        if(n1 != n2)return false;
+        unordered_map<char,int>m;
+
+        for(int i = 0; i < n1; i++){
+            m[s[i]]++;
+        }
+        for(int i = 0; i < n2; i++){
+            m[t[i]]--;
+        }
+
+        for(auto it = m.begin(); it != m.end(); it++){
+            if(it->second != 0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
