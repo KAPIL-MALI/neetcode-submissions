7109fc0100b2e2a0770df4bef7aefbcdf35080cd
KAPIL <kapilmali7789@gmail.com>
2026-05-27 17:13:51 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+        int n = s.size(),i = 0;
+
+        while(i < n){
+            if(s[i] == '[' || s[i] == '{' || s[i] == '('){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                else if(st.top() == '[' && s[i] == ']'){
+                    st.pop();
+                }
+                else if(st.top() == '(' && s[i] == ')'){
+                    st.pop();
+                }
+                else if(st.top() == '{' && s[i] == '}'){
+                    st.pop();
+                }
+                else{// FALSE PAIR
+                    return false;
+                }
+            }
+            i++;
+        }
+        if(st.size() == 0)return true;
+        else return false;
+    }
+};
